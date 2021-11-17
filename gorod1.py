d4b40b00e8676649fa8df6d1bb321f7cc65fe4e3
Ivan <aagrach@gmail.om>
2021-11-17 13:40:33 +0300

    17,11

--- gorod1.py
+++ gorod1.py
@@ -3,56 +3,96 @@ from random import randint
 
 class Man:
 
-
     def __init__(self, name):
         self.name = name
         self.fullness = 50
-        self.food = 50
-        self.money = 0
+        self.house = None
 
     def __str__(self):
-        return 'Я - {}, сытость {}, еды осталось {}, денег осталось  {}'.format(
-            self.name, self.fullness, self.food, self.money)
-
+        return 'Я - {}, {} сытость '.format(self.name, self.fullness,)
 
     def eat(self):
-        if self.food > 10:
+        if self.house.food >= 10:
             print('{} поел'.format(self.name))
-            self.fullness += 10
-            self.food -= 10
-            self.money -=25
+            self.fullness += 20
+            self.house.food -= 10
+
         else:
             print('У {} нет еды'.format(self.name))
 
-
-
-
     def work(self):
         print('{} идет на работу'.format(self.name))
-        self.money += 50
+        self.house.money += 50
         self.fullness -= 10
 
     def play_DOTA(self):
         self.fullness -= 10
         print('{} играл в доту целый день'.format(self.name))
 
+    def go_to_shop(self):
+        self.house.food += 20
+        self.house.money -= 10
+        print('{} cходил в магазин'.format(self.name))
+
+
     def act(self):
-        if self.fullness <=0:
+        if self.fullness == 0:
             self.eat()
-        dice = randint(1, 6)
-        if dice == 1:
+        elif self.house.money < 10:
             self.work()
-        elif dice ==2:
-            self.eat()
+        elif self.house.food < 10:
+            self.go_to_shop()
         else:
-            self.play_DOTA()
+            dice = randint(1, 6)
+            if dice == 1:
+                self.work()
+            elif dice == 2:
+                self.eat()
+            else:
+                self.play_DOTA()
+
+    def go_to_the_house(self, house):
+        self.house = house
+        self.fullness -=10
+        print('{} заехал в дом'.format(self.name))
 
 
-man_Vasia = Man(name='Вася')
-print(man_Vasia)
+class House:
+    def __init__(self):
+        self.food = 10
+        self.money = 50
+    def __str__(self):
+        return 'В доме - {} еды, - {} денег '.format(
+            self.food, self.money)
+
+
+
+
+
+citizens = [
+    Man(name= 'Бивис'),
+    Man(name= 'Батхед'),
+    Man(name='Кенни')
+
+]
+my_sweet_home = House()
+
+for citizen in citizens:
+    citizen.go_to_the_house(house=my_sweet_home)
+
+
+bivis = Man(name='Бивис')
+butthed = Man(name='Бадхед')
+
+
 days = 0
-for i in range(1,30):
-    man_Vasia.act()
-    days +=1
-    print(man_Vasia)
-    print(days)
+for i in range(1, 100):
+    print('\nXXXXXXXXXXXXXXX    {}  день  XXXXXXXXXXXXXXX\n'.format(days))
+    for citizen in citizens:
+        citizen.act()
+    print('______________ В конце дня _____________')
+    for citizen in citizens:
+        print(citizen)
+
+    print(my_sweet_home)
+    days += 1
